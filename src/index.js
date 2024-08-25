2606461bcda76d66bfb3f40958e6656006ee892d
oluchi342 <blackfloweroops@gmail.com>
2024-08-25 15:40:43 -0700

    Updated the informations using axios on index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ function displayWeather(response) {
     let tempsElement = document.querySelector("#temperature-value");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity")
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date;
+
+
     cityElement.innerHTML = response.data.city;
     tempsElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satruday",];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+       minutes = `0${minutes}`; 
+    }
+
+    return `${day} ${hours}:${minutes}`;
+    
+
+}
 
 function searchCity(city) {
     let apiKey = "o4bcac1at7e73505ab4974f6a544518b";

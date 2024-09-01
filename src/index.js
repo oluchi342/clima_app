140e5cf2753f1cf0be95ac5f7d782756d1d6c0f7
oluchi342 <blackfloweroops@gmail.com>
2024-09-01 10:04:31 -0700

    Added live data forecast data from javaScript

--- src/index.js
+++ src/index.js
@@ -50,6 +50,12 @@ function searchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat", "Sun"];
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "o4bcac1at7e73505ab4974f6a544518b";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -59,21 +65,23 @@ function getForecast(city) {
 function displayForecast(response) {
     console.log(response.data);
 
-    let days = ["Tue", "Wed", "Thu", "Sat"];
+    
     let forecastHtml = "";
 
-    days.forEach(function(day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
         forecastHtml = 
         forecastHtml + 
         ` <div class="daily-forecast-day">
-        <div class="daily-forecast-date">${day}</div>
-        <div class="daily-forecast-icon">🌦️</div>
+        <div class="daily-forecast-date">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}"class="daily-forecast-icon"  />
         <div class="daily-forecast-temperatures">
-        <div class="daily-forecast-temperature"><strong>15⁰</strong></div> 
-        <div class="daily-forecast-temperature">9⁰</div>
+        <div class="daily-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}⁰</strong></div> 
+        <div class="daily-forecast-temperature">${Math.round(day.temperature.minimum)}⁰</div>
         </div>
         </div>
         `;
+        }
     });
     let forecastElement = document.querySelector("#forecast")
     forecastElement.innerHTML = forecastHtml;
